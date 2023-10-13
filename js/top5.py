81decc6e25515e0b05bd7cdac64d731c8bc6850e
Michaella <mchu0030@student.monash.edu>
2023-10-13 17:42:07 +1100

    packed bubble

--- js/top5.py
+++ js/top5.py
@@ -1,14 +1,38 @@
+import math
+country_dict = {
+    "United States" : [0,0],
+    "Germany" : [0,0],
+    "China" : [0,0],
+    "India" : [0,0],
+    "United Kingdom" : [0,0]
+
+}
 with open ("js/billionaires.csv" , "r") as f,open ("js/top5.csv" , "w") as o:
-    for line in f:
-        o.write(line)
-        break
     for line in f:
         line = line.strip()
         line2 = line.split(",")
         country = line2[5]
-        if country in ["United States" , "China", "India", "United Kingdom","Germany"]:
-            line = line + '\n'
-            o.write(line)
+        gender = line2[13]
+        if country in country_dict and gender == 'M':
+            country_dict[country][0] += 1
+        elif country in country_dict and gender == 'F':
+            country_dict[country][1] += 1
+    for country in country_dict.values():
+        for i in range(len(country)):
+            values = math.ceil(country[i] / 10)
+            country[i] = values
+    print(country_dict)
+    for country in country_dict:
+        for i in range(len(country_dict[country])):
+            for j in range(country_dict[country][i]):
+                if i == 0 :
+                    line = country + "," + "M\n"
+                    o.write(line)
+                else:
+                    line = country + "," + "F\n"
+                    o.write(line)
+        
+
 
     
   
