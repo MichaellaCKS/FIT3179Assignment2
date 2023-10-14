fffcae7808762faf7770fc48976b242dd6637dc1
Michaella <mchu0030@student.monash.edu>
2023-10-14 20:42:20 +1100

    index.html

--- js/vega.js
+++ js/vega.js
@@ -2,7 +2,8 @@ var specVis1 = "./js/cloropeth.vg.json";
 var specVis2 = "./js/line.vg.json";
 var specVis3 = "./js/bubble.vg.json";
 var specVis4 = "./js/symbolmap.vg.json";
-// var specVis5 = "https://raw.githubusercontent.com/MichaellaCKS/FIT3179Assignment2/main/js/line.vg.json";
+var specVis5 = "./js/treemap.vg.json";
+
 
 vegaEmbed('#choropeth', specVis1, {
     "actions": false
@@ -17,5 +18,9 @@ vegaEmbed('#line', specVis2, {
 vegaEmbed('#bubble', specVis3, { "actions": false });
 
 vegaEmbed('#symbol', specVis4, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#treemap', specVis5, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
